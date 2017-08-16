3ff20938e037a2c7b3016d488e81d36e10d17213
Tassilo Weidner <tassilo.weidner@camunda.com>
2017-08-16 19:49:22 +0200

    feat(tasklist): add possibility to swipe for further actions

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,12 +1,18 @@
+import Swipeable from 'react-native-swipeable';
 import React from 'react';
-import { View, FlatList, RefreshControl, StyleSheet, Text } from 'react-native';
+import { View, FlatList, RefreshControl, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
 
+const leftContent = <Text>Pull to activate</Text>;
+const rightButtons = [
+  <Text>Button 1</Text>,
+  <Text>Button 2</Text>
+];
 
 export default class TaskList extends React.Component {
 
   _keyExtractor = (item, index) => item.id;
 
-  _renderItem = ({item}) => <Text style={styles.item} key={item.id}>{item.name}</Text>
+  _renderItem = ({item}) => <Swipeable style={styles.item} leftContent={leftContent} rightButtons={rightButtons}><Text key={item.id}>{item.name}</Text></Swipeable>;
 
   constructor(props) {
     super(props);
@@ -62,6 +68,7 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 10,
+    width:200,
     flex: 1
   }
 });
